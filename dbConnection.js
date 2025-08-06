cb3f6b070778f0377bbf16b3dbd4290334a393a8
DEVVRATSHARMA-ZZZ <devzzzwebhosting@gmail.com>
2025-08-06 16:08:07 +0530

    update

--- dbConnection.js
+++ dbConnection.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 mongoose
   .connect(
-    "mongodb+srv://dev1234567zzz:9258013998@cluster0.ytokqug.mongodb.net"
+    "mongodb+srv://dev1234567zzz:9258013998@cluster0.ytokqug.mongodb.net/StudentAttendanceSystem"
   )
   .then(() => console.log(`mongodb StudentSystem connected`))
   .catch((err) => console.log(`mongodb StudentSystem error ${err}`));
